79f0d2ece28ab2dfdc82295ee26757ba6d0f7e66
Suhani <kawadesuhani02@gmail.com>
2025-11-12 18:14:19 +0530

    Add files via upload

---
+++ frontend/src/auth/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { AuthContext } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { token } = useContext(AuthContext);
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute; 
