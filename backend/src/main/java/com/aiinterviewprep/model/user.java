3f30e6b394222328f7b57c345e6acc886ac05f39
siddhu3000 <161158931+siddhu3000@users.noreply.github.com>
2025-11-12 18:20:18 +0530

    Create User model for MongoDB with Lombok annotations

---
+++ backend/src/main/java/com/aiinterviewprep/model/user.java
@@ -0,0 +1,18 @@
+package com.aiinterviewprep.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String passwordHash;
+} 
