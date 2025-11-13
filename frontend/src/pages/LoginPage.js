e0d4f169a07c5bf366f26166e942c8891cca5d2e
Aniket Dhakane <162018654+aniket050603@users.noreply.github.com>
2025-11-13 19:00:50 +0530

    Create LoginPage.js

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from "../components/LoginForm";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => (
+  <div>
+    <LoginForm />
+  </div>
+);
+
+export default LoginPage; 
